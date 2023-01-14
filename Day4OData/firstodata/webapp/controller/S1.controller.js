27ff3100bf3a4f1d4f2bfeafd625dbf300a76d7f
srinivassaluru <salurusrinivasarao@gmail.com>
2023-01-14 14:21:29 +0530

    First OData Prohect

---
+++ Day4OData/firstodata/webapp/controller/S1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("com.rebit.ui5.firstodata.controller.S1", {
+            onInit: function () {
+
+            }
+        });
+    });
---
+++ Day4OData/firstodata/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/rebit/ui5/firstodata/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
