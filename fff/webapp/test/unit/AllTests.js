7c7cafcaf3431e85b5168780559f10748c764234
srinivassaluru <salurusrinivasarao@gmail.com>
2023-01-14 12:12:53 +0530

    Delete Code

---
+++ fff/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"fff/test/unit/controller/S1.controller"
+], function () {
+	"use strict";
+});
--- leaveapplicationday4/webapp/controller/S1.controller.js
+++ leaveapplicationday4/webapp/controller/S1.controller.js
@@ -12,7 +12,37 @@ sap.ui.define([
 
             lifeSaver:formatter, //  create a global reference 
             onInit: function () {
-
+                this.oView = this.getView();
+            },
+            handleSelectionChange:function(){
+                // alert("Eve");
+                // var oView = this.getView(); // 1. setModel() 2.byId() 3. getModel()
+                this.toggleDeleteBtn(true);
+            },
+            // Delete Btn Event
+            handleDelete: function(){
+                var listObject = this.oView.byId("listId");
+                //Select Single Delete 
+                var sPath = listObject.getSelectedContextPaths()[0];
+                var sIndex = sPath.substr(sPath.lastIndexOf("/")+1);
+                var loadedModel = this.oView.getModel("jsonModelData");
+                var loadServiceData = loadedModel.getData();
+                var leavesData = loadServiceData.leavesData;
+               leavesData.splice(sIndex, 1);
+                if(leavesData.length === 0){
+                    this.toggleDeleteBtn(false);
+                }
+                loadedModel.refresh();
+                // var selectedItems = listObject.get
+                // var selectedItemContexts = listObject.getSelectedContextPaths();
+                // 1. Loop All entreis . 
+                // 2. Get Indexs 
+                // 3. Remove from list array 
+                // 4. model refresh
+            },
+            toggleDeleteBtn:function(sFlag){
+                var deleteBtnObject = this.oView.byId("deleteBtn");
+                deleteBtnObject.setEnabled(sFlag);
             }
         });
     });
